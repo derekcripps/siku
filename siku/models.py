1186cab63aaf62d614bcae4f00b56c2fd30008e7
Derek Cripps <DerekCripps@gmail.com>
2017-10-22 17:04:29 -0400

    Oct 22 R1

--- siku/models.py
+++ siku/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE())
+    release_date = models.DateField()
+    rating = models.IntegerField()
+    image = models.ImageField(upload_to='albums')
+
+    def __str__(self):
+        return self. name
+
+
+class Track(models.Model):
+    track = models.IntegerField()
+    name = models.CharField(max_length=200)
+    album = models.ManyToManyField(Album)
+    track_location = models.FileField(upload_to='music')
+
+    def __str__(self):
+        return self.name
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=200)
+    track = models.ForeignKey(Track)
+
+    def __str__(self):
+        return self.name
+
+
+
